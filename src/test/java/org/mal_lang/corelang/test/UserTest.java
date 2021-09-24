8672c380a8856d77b7dffd4bcab814998c6478a8
Andrei Buhaiu <andreibuhaiu@gmail.com>
2021-09-24 14:19:01 +0200

    Remove reverse takeover attack steps and rework user tests to reflect that and account for reverse reach instead

--- src/test/java/org/mal_lang/corelang/test/UserTest.java
+++ src/test/java/org/mal_lang/corelang/test/UserTest.java
@@ -9,82 +9,111 @@ public class UserTest extends CoreLangTest {
         public final User user = new User("user");
 
         public final Credentials credentials = new Credentials("credentials");
-        public final Application application = new Application("application");
+        public final Application application;
         public Identity identity;
 
-        public UserTestModel() {
+        public UserTestModel(boolean hasCredentials,
+                boolean reverseReachable)
+        {
+          application = new Application("application", false,
+                  !reverseReachable, false);
           identity = new Identity("identity", false);
           user.addUserIds(identity);
-          identity.addCredentials(credentials);
+          if (hasCredentials)
+          {
+            identity.addCredentials(credentials);
+          }
           identity.addExecPrivApps(application);
         }
 
         public void addAttacker(Attacker attacker) {
           attacker.addAttackPoint(user.attemptSocialEngineering);
+          attacker.addAttackPoint(application.attemptReverseReach);
         }
 
-    }
-
-    private static class UserTestModelNoCreds {
-
-        public final User user = new User("user");
-
-        public final Application application = new Application("application",
-                false, true, false);
-        public Identity identity;
-
-        public UserTestModelNoCreds() {
-          identity = new Identity("identity", false);
-          user.addUserIds(identity);
-          identity.addExecPrivApps(application);
-        }
-
-        public void addAttacker(Attacker attacker) {
-          attacker.addAttackPoint(user.attemptSocialEngineering);
-        }
     }
 
     @Test
-    public void testPhishing() {
+    public void testPhishingWithCredsNotReverseReachable() {
         printTestName(Thread.currentThread().getStackTrace()[1].getMethodName());
-        var model = new UserTestModel();
+        var model = new UserTestModel(true, false);
 
         var attacker = new Attacker();
         model.addAttacker(attacker);
         attacker.attack();
 
         model.user.phishUser.assertCompromisedInstantaneously();
-        model.user.phishUser.assertCompromisedInstantaneously();
         assertReached(model.user.credentialTheft);
-        assertReached(model.user.reverseTakeover);
 
         assertReached(model.credentials.credentialTheft);
+        assertReached(model.credentials.use);
 
-        assertReached(model.application.networkConnect);
         assertReached(model.identity.assume);
 
+        model.application.networkConnect.assertUncompromised();
+        model.application.fullAccess.assertUncompromised();
+    }
+
+    @Test
+    public void testPhishingNoCredsNotReverseReachable() {
+        printTestName(Thread.currentThread().getStackTrace()[1].getMethodName());
+        var model = new UserTestModel(false, false);
+
+        var attacker = new Attacker();
+        model.addAttacker(attacker);
+        attacker.attack();
+
+        model.user.phishUser.assertCompromisedInstantaneously();
+        assertReached(model.user.credentialTheft);
+
+        model.credentials.use.assertUncompromised();
+
+        assertReached(model.identity.assume);
+
+        model.application.networkConnect.assertUncompromised();
+        model.application.fullAccess.assertUncompromised();
+    }
+
+    @Test
+    public void testPhishingWithCredsReverseReachable() {
+        printTestName(Thread.currentThread().getStackTrace()[1].getMethodName());
+        var model = new UserTestModel(true, true);
+
+        var attacker = new Attacker();
+        model.addAttacker(attacker);
+        attacker.attack();
+
+        model.user.phishUser.assertCompromisedInstantaneously();
+        assertReached(model.user.credentialTheft);
+
+        assertReached(model.credentials.credentialTheft);
+        assertReached(model.credentials.use);
+
+        assertReached(model.identity.assume);
+
+        assertReached(model.application.networkConnect);
         assertReached(model.application.fullAccess);
 
     }
 
     @Test
-    public void testPhishingNoCreds() {
+    public void testPhishingNoCredsReverseReachable() {
         printTestName(Thread.currentThread().getStackTrace()[1].getMethodName());
-        var model = new UserTestModelNoCreds();
+        var model = new UserTestModel(false, true);
 
         var attacker = new Attacker();
         model.addAttacker(attacker);
         attacker.attack();
 
         model.user.phishUser.assertCompromisedInstantaneously();
-        model.user.phishUser.assertCompromisedInstantaneously();
         assertReached(model.user.credentialTheft);
-        assertReached(model.user.reverseTakeover);
 
-        model.application.networkConnect.assertUncompromised();
+        model.credentials.use.assertUncompromised();
+
         assertReached(model.identity.assume);
 
-        model.application.fullAccess.assertUncompromised();
+        assertReached(model.application.networkConnect);
+        assertReached(model.application.fullAccess);
     }
 
 }
