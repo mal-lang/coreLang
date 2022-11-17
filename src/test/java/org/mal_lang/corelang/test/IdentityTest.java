c99664d9c39b85bf54a696109c362b21b6d05251
Andrei Buhaiu <buhaiu@kth.se>
2022-11-17 13:11:57 +0100

    Remove unnecessary attack steps and defences.

--- src/test/java/org/mal_lang/corelang/test/IdentityTest.java
+++ src/test/java/org/mal_lang/corelang/test/IdentityTest.java
@@ -62,7 +62,7 @@ public class IdentityTest extends CoreLangTest {
         var model = new IdentityTestMultipleCredentialsModel();
 
         var attacker = new Attacker();
-        attacker.addAttackPoint(model.creds1.attemptUse);
+        attacker.addAttackPoint(model.creds1.use);
         attacker.attack();
 
         model.identity.successfulAssume.assertCompromisedInstantaneously();
